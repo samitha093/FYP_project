ced1d8e9f0beae629e9d3358830768e809cb0615
lakshan Pathiraja <lakshanSpathiraja@proton.me>
2023-04-04 11:47:43 +0530

    bg_process create for parent cart

--- parent_cart/main.py
+++ parent_cart/main.py
@@ -1,5 +1,8 @@
 import sys
 import os
+import threading
+import time
+import psutil
 
 # Get the path to the root directory
 root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -15,7 +18,26 @@ def child_cart():
     mainFunc("PARENT")
 
 def Bridge():
-    bidge_server()
+    bidge_server(host = 'http://172.20.2.3', boostrap_host ='127.0.0.1')
 
 if __name__ == '__main__':
+    cart_thread = threading.Thread(target=Bridge)
+    cart_thread.start()
+
+    # Wait for the TCP server process to start
+    while True:
+        # Check if the TCP server process is running
+        for proc in psutil.process_iter(['pid', 'name']):
+            if proc.info['name'] == 'tcp_server' and proc.info['pid'] != os.getpid():
+                # If the process is running, break out of the loop
+                break
+        else:
+            # If the process is not running, wait for 1 second and try again
+            time.sleep(1)
+            continue
+
+        # If the process is running, break out of the loop
+        break
+
+    # Start child_cart()
     child_cart()
