b06d2b38044a85c76e2bfc9d5eeb1af9d4f0e668
lakshan Pathiraja <lakshanSpathiraja@proton.me>
2023-04-04 10:18:33 +0530

    ignore update

--- parent_cart/child_cart/sync.py
+++ parent_cart/child_cart/sync.py
@@ -1,18 +1,15 @@
 import sys
 import os
-from threading import Thread
 
 # Get the path to the root directory
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 
 # Add the root and client4 directories to the Python path
 sys.path.insert(0, root_path)
 
 
 # Import the modules
-from child_cart.ui.Flask import *
+from child_cart.main import *
 
 if __name__ == '__main__':
-    t = Thread(target=app.run, kwargs={'port': 5001})
-    t.start()
-    backgroudNetworkProcess()
+    mainFunc()
