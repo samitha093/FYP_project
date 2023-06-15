3f1238aa536d44f1a49fa6ac63c5d9b12d906e36
lakshan Pathiraja <lakshanSpathiraja@proton.me>
2023-06-15 13:03:31 +0530

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "."))
 sys.path.insert(0, root_path)
 
 from parent_cart.main import *
