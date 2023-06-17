dbe1dba01f5f44457b1deef9a77955c15264724f
IsuruLakshan170 <lakshanisuru170@gmail.com>
2023-06-17 23:20:36 +0530

    apply log in table

--- web_app/src/components/dashbord/log.tsx
+++ web_app/src/components/dashbord/log.tsx
@@ -1,12 +1,90 @@
 import React, { useEffect, useState } from 'react';
+import { Container, Table } from 'react-bootstrap';
 
 const Log = () => {
-  const [logData, setLogData] = useState<Array<{
-    iteration: number;
-    localModel: { id: string; value: boolean; accuracy: number };
-    receivedModel: Array<{ id: string; value: boolean; accuracy: number }>;
-    aggregatedModel: { id: string; value: boolean; accuracy: number };
-  }>>([]);
+
+  //this is correct
+  // const [logData, setLogData] = useState<Array<{
+  //   iteration: number;
+  //   localModel: { id: string; value: boolean; accuracy: number };
+  //   receivedModel: Array<{ id: string; value: boolean; accuracy: number }>;
+  //   aggregatedModel: { id: string; value: boolean; accuracy: number };
+  // }>>([]);
+
+//this is for demo only
+    const [logData, setLogData] = useState([
+      {
+        iteration: 1,
+        localModel: {
+          id: "0001",
+          value: "True",
+          accuracy: 0.58
+        },
+        receivedModel: [
+          {
+            id: "0001",
+            value: "True",
+            accuracy: 0.88
+          },
+          {
+            id: "0002",
+            value: "True",
+            accuracy: 0.88
+          },
+          {
+            id: "0003",
+            value: "False",
+            accuracy: 0.88
+          },
+          {
+            id: "0004",
+            value: "True",
+            accuracy: 0.88
+          }
+        ],
+        aggregatedModel: {
+          id: "0005",
+          value: "True",
+          accuracy: 0.92
+        }
+      },
+      {
+        iteration: 1,
+        localModel: {
+          id: "0001",
+          value: "False",
+          accuracy: 0.58
+        },
+        receivedModel: [
+          {
+            id: "0001",
+            value: "True",
+            accuracy: 0.88
+          },
+          {
+            id: "0002",
+            value: "True",
+            accuracy: 0.88
+          },
+          {
+            id: "0003",
+            value: "False",
+            accuracy: 0.88
+          },
+          {
+            id: "0004",
+            value: "True",
+            accuracy: 0.88
+          }
+        ],
+        aggregatedModel: {
+          id: "0005",
+          value: "True",
+          accuracy: 0.92
+        }
+      }
+    ]);
+  
 
   useEffect(() => {
     fetchData();
@@ -23,45 +101,60 @@ const Log = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome to the Log Dashboard!</h1>
-      {logData.length ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Iteration</th>
-              <th>Local Model Id</th>
-              <th>Local Model Accuracy</th>
-              <th>Received Model Id</th>
-              <th>Received Model Accuracy</th>
-              <th>Aggregated Model Id</th>
-              <th>Aggregated Model Accuracy</th>
-            </tr>
-          </thead>
-          <tbody>
-            {logData.map((log, index) => (
-              <tr key={index}>
-                <td>{log.iteration}</td>
-                <td>{log.localModel.id}</td>
-                <td>{log.localModel.accuracy}</td>
-                <td>
-                  <ul>
-                    {log.receivedModel.map((model, i) => (
-                      <li key={i}>{`${model.id} (${model.accuracy})`}</li>
-                    ))}
-                  </ul>
-                </td>
-                <td>{log.aggregatedModel.id}</td>
-                <td>{log.aggregatedModel.accuracy}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>Loading log data...</p>
-      )}
-    </div>
-  );
+    <Container>
+    <h1 className="text-center text-primary fw-bold">Welcome to the Log Dashboard!</h1>
+
+    {logData.length ? (
+      <Table striped bordered responsive className="mt-3" style={{ margin: '0 auto' }}>
+  <thead className="thead-dark">
+    <tr className="text-center align-middle">
+      <th>Iteration</th>
+      <th>Local Model Id</th>
+      <th>Local Model Accuracy</th>
+      <th>Received Model Id</th>
+      <th>Received Model Accuracy</th>
+      <th>Received Model Value</th>
+      <th>Aggregated Model Id</th>
+      <th>Aggregated Model Accuracy</th>
+    </tr>
+  </thead>
+  <tbody>
+    {logData.map((log, index) => (
+      <React.Fragment key={index}>
+        <tr>
+          <td>{log.iteration}</td>
+          <td>{log.localModel.id}</td>
+          <td>{log.localModel.accuracy}</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td>{log.aggregatedModel.id}</td>
+          <td>{log.aggregatedModel.accuracy}</td>
+        </tr>
+        {log.receivedModel.map((model, i) => (
+          <tr key={i}>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td style={{ backgroundColor: model.value === "False" ? "red" : "white" }}>{model.id}</td>
+            <td style={{ backgroundColor: model.value === "False" ? "red" : "white" }}>{model.accuracy}</td>
+            <td style={{ backgroundColor: model.value === "False" ? "red" : "white" }}>{model.value}</td>
+            <td></td>
+            <td></td>
+          </tr>
+        ))}
+      </React.Fragment>
+    ))}
+  </tbody>
+</Table>
+
+
+    ) : (
+      <p>Loading log data...</p>
+    )}
+  </Container>
+);
 };
+
 
 export default Log;
