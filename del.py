7f63bac518d6e7f5f7fcabf6be2fd72321c89826
Isuru lakshan ketawala <82941889+IsuruLakshan170@users.noreply.github.com>
2023-07-20 10:29:03 +0530

    delete inside dist folder .exe

--- del.py
+++ del.py
@@ -6,20 +6,18 @@ directory = 'web_app'
 os.chdir(directory)
 
 # Run the 'yarn build' command
+
 build_command = 'yarn build'
 subprocess.run(build_command, shell=True)
 
 # Change the directory back to the parent directory
 os.chdir('..')
 
-
 import os
 import shutil
 
-
-folders_to_delete = ["build", "dist"]
-files_to_delete = [file for file in os.listdir(".") if file.endswith(".spec")]
-
+#delete build
+folders_to_delete = ["build"]
 for folder_name in folders_to_delete:
     if os.path.exists(folder_name) and os.path.isdir(folder_name):
         # Directory exists, so delete it recursively
@@ -28,10 +26,26 @@ for folder_name in folders_to_delete:
     else:
         print(f"The '{folder_name}' folder does not exist.")
 
+
+#delete .spec files
+files_to_delete = [file for file in os.listdir(".") if file.endswith(".spec")]
 for file_name in files_to_delete:
     if os.path.isfile(file_name):
         # File exists, so delete it
         os.remove(file_name)
+        print(f"The '{file_name}' file has been deleted.")
+    else:
+        print(f"The '{file_name}' file does not exist.")
+
+#delete .exe files inside dist
+directoryDel="./dist"
+files_to_delete = [file for file in os.listdir(directoryDel) if file.endswith(".exe")]
+for file_name in files_to_delete:
+    # Construct the full path to the file
+    file_path = os.path.join(directoryDel, file_name)
+    if os.path.isfile(file_path):
+        # File exists, so delete it
+        os.remove(file_path)
         print(f"The '{file_name}' file has been deleted.")
     else:
         print(f"The '{file_name}' file does not exist.")
