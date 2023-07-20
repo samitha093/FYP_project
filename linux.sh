8439e1c2f803c3231d662cce21b06bd83d15fd29
lakshan Pathiraja <lakshanSpathiraja@proton.me>
2023-07-20 11:48:09 +0530

    update

--- linux.sh
+++ linux.sh
@@ -11,9 +11,7 @@ cd ..
 echo "😎 => copying web app to child cart ..."
 cp -r web_app/dist/* child_cart/api/templates
 echo "😎 => creating python binary file for parent..."
-# pyinstaller --onefile --add-data "web_app/dist:web_app/dist" --name PARENT_CART_LINUX.bin P_main.py
-pyinstaller --onefile --add-data  "child_cart/api/templates:child_cart/api/templates"  --name PARENT_CART_LINUX.bin P_main.py
+pyinstaller --onefile --add-data  "child_cart/api/templates:child_cart/api/templates"  --name P_main.bin P_main.py
 echo "😎 =>creating python binary file for child..."
-# pyinstaller --onefile --add-data "web_app/dist:web_app/dist" --name CHILD_CART_LINUX.bin C_main.py
-pyinstaller --onefile --add-data "child_cart/api/templates:child_cart/api/templates"  --name CHILD_CART_LINUX.bin C_main.py
+pyinstaller --onefile --add-data "child_cart/api/templates:child_cart/api/templates"  --name C_main.bin C_main.py
 echo "😎 => Build is Sucessfully completed!"
